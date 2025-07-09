a721c090fb9f8f2f6e48ff4e386af41d20196b77
l-ryuya <l-ryuya@users.noreply.github.com>
2025-07-09 06:45:56 +0000

    deploy: f5f032ffa0d3930371fc8eeef09e7ee23d093e3c

--- frontend/vue/src/router/index.js
+++ frontend/vue/src/router/index.js
@@ -5,12 +5,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/github_actions_test/out/',
       name: 'home',
       component: HomeView,
     },
     {
-      path: '/about',
+      path: '/github_actions_test/out/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
